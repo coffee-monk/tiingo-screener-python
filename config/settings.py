76874e944bafa7c18d08d97b0acca50116d359de
coffee-monk <coffeemonk@hotmail.com>
2025-08-09 11:49:54 -0400

    tweaks

--- config/settings.py
+++ config/settings.py
@@ -18,4 +18,5 @@ SCREENSHOTS_DIR = PROJECT_ROOT / "data" / "screenshots"
 
 # List Directories
 
-TICKERS_LIST    = PROJECT_ROOT / 'src/fetch_data/ticker_lists/TSX.csv'
+# TSX, QQQ, NASDAQ
+TICKERS_LIST    = PROJECT_ROOT / 'src/fetch_data/ticker_lists/QQQ.csv'
