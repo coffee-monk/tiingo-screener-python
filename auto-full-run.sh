557f37ef7727746441c3ce565d31b857d3f2d5ec
mjc <coffeemonk@hotmail.com>
2025-08-24 12:50:53 -0400

    Auto-commit: Daily tickers/indicators for 2025-08-24

--- auto-full-run.sh
+++ auto-full-run.sh
@@ -8,6 +8,8 @@ touch "script_run_$(date +'%Y-%m-%d_%H-%M-%S').log"
 
 echo "DELETE" | python app.py --full-run
 
+toucn "script_run_FINAL_$(date +'%Y-%m-%d_%H-%M-%S').log"
+
 git add .
 git commit -m "Auto-commit: Daily tickers/indicators for $(date +'%Y-%m-%d')"
 git push
